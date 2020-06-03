e54a6fda45a9fcd6ab701f4a512f645287f7120f
subashlimbu <prajan_l@hotmail.com>
2020-06-03 19:46:36 +0100

    env sorted

--- django_project/settings.py
+++ django_project/settings.py
@@ -139,11 +139,9 @@ LOGIN_URL = 'login'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-# EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
-EMAIL_HOST_USER = 'subashlimbu700@gmail.com'
-EMAIL_HOST_PASSWORD = 'bjfsirkqqbpabnmo'
 EMAIL_USE_TLS = True
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
