e27af13f65f5de309c167466d593a72f40eab777
subashlimbu <prajan_l@hotmail.com>
2020-06-03 22:00:44 +0100

    heroku env

---
+++ django_project/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/subashlimbu/development/project-x/django_project/django_project/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
